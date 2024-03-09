39cd23b9876a7d0e738cb5be08606693c3c42a25
x25099 <114368056+x25099@users.noreply.github.com>
2024-03-09 11:00:05 -0300

    CSS - parte 3 - projetos parte 1
    Meu projetos com CSS feito no curso de programação da Grau Técnico.

---
+++ CSS - parte 3 - projetos 1/modelo_final/Techtudo/Untitled-1.js
@@ -0,0 +1,13 @@
+alert("BOAS VINDAS AO SISTEMA - DEPARTAMENTO PESOAL FACIL")
+let idade = pronpt("QUANTOS ANOS VC TEM")
+let (idade >= 65) {
+     alert(Vc tem a idade minima permitida)
+}
+let idade = pronpt("QUANTOS ANOS VC TEM TRABALHANDO ")
+let (trabalho >= 30 ) {
+     alert(Vc tem idade minima de trabalho prestado permitida)
+}
+let idade = pronpt("VOCE DESEJA PEDIR A APOSENTADORIA ")
+let (idade >= 65 ) {
+    alert(VOCE AGORA TEM A APOSSENTADORIA)
+}
